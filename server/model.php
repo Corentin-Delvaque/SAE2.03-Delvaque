bc95140da224491a556be3d92b682404c5bf9763
Delvaque Corentin <delvaque1@mmi-dev.unilim.fr>
2025-04-03 09:29:17 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "delvaque1");
+define("DBLOGIN", "delvaque1");
+define("DBPWD", "delvaque1");
