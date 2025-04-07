283aef8a85713dc106a59306713bc8325f0c7e57
Corentin Delvaque <corentin.delvaque@icloud.com>
2025-04-07 18:22:34 +0200

    Iteration 3 : Mise en place de l'affichage des informations des differents anim

--- app/component/AnimeCard/script.js
+++ app/component/AnimeCard/script.js
@@ -3,15 +3,31 @@ let template = await templateFile.text();
 
 let AnimeCard = {};
 
-AnimeCard.format = function (Trailer, Affiche, Name) {
+AnimeCard.format = function (
+  Trailer,
+  Affiche,
+  Description,
+  Year,
+  Length,
+  Min_Age,
+  Name,
+  id
+) {
   let html = template;
 
   let truncatedName = Name.length > 20 ? Name.substring(0, 20) + "..." : Name;
 
   html = html.replace("{{Trailer}}", Trailer);
   html = html.replace("{{Affiche}}", Affiche);
+  html = html.replace("{{Description}}", Description);
+  html = html.replace("{{Year}}", Year);
+  html = html.replace("{{Length}}", Length);
+  html = html.replace("{{Min_Age}}", Min_Age);
   html = html.replace("{{Name}}", truncatedName);
   html = html.replace("{{Name}}", truncatedName);
+  html = html.replace("{{Name}}", truncatedName);
+  html = html.replace("{{Name}}", truncatedName);
+  html = html.replace("{{id}}", id);
   return html;
 };
 
